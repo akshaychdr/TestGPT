b68d49460bfbb5059fd49fe179fb7dd00990bf3b
akshaychdr <129782806+akshaychdr@users.noreply.github.com>
2023-04-27 18:14:17 +0530

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -32,7 +32,7 @@ export const makeChain = (
   onTokenStream?: (token: string) => void,
 ) => {
   const questionGenerator = new LLMChain({
-    llm: new OpenAIChat({ temperature: 0.2 }),
+    llm: new OpenAIChat({ temperature: 0.15 }),
     prompt: CONDENSE_PROMPT,
   });
   const docChain = loadQAChain(
